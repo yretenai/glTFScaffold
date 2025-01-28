f747db548b0abb3605c59c471d1f23b500fabca0
Ada <yretenai@gmail.com>
2025-01-28 01:39:03 +0000

    chore: add extension helpers

---
+++ Extensions.cs
@@ -0,0 +1,21 @@
+using System.Numerics;
+
+namespace GLTF.Scaffold;
+
+public static class Extensions {
+	public static List<double> ToGLTF(this Vector2 vector) {
+		return [vector.X, vector.Y];
+	}
+
+	public static List<double> ToGLTF(this Vector3 vector) {
+		return [vector.X, vector.Y, vector.Z];
+	}
+
+	public static List<double> ToGLTF(this Vector4 vector) {
+		return [vector.X, vector.Y, vector.Z, vector.W];
+	}
+
+	public static List<double> ToGLTF(this Quaternion quaternion) {
+		return [quaternion.X, quaternion.Y, quaternion.Z, quaternion.W];
+	}
+}
